671944ac23ad6daa4babf705709b1d5357d2c8d5
EgorSivenko <egor.siv20@gmail.com>
2024-01-09 18:17:25 +0200

    Initial Commit

---
+++ src/main/java/ua/nure/cpp/sivenko/practice6/Practice6Application.java
@@ -0,0 +1,13 @@
+package ua.nure.cpp.sivenko.practice6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practice6Application.class, args);
+	}
+
+}
