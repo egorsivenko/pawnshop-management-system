e6d6bc270b1b5498bd317de9bf3c74a81a24e199
EgorSivenko <egor.siv20@gmail.com>
2024-02-01 19:27:58 +0200

    Database migrations added with Flyway

---
+++ src/main/resources/db/migration/V2__pawn_transactions_before_insert.sql
@@ -0,0 +1,27 @@
+DELIMITER $$
+
+CREATE TRIGGER pawn_transactions_before_insert
+    BEFORE INSERT ON pawn_transactions
+    FOR EACH ROW
+BEGIN
+    DECLARE item_market_price_max DECIMAL(9, 2);
+    DECLARE item_market_price_min DECIMAL(9, 2);
+    DECLARE item_category VARCHAR(50);
+    DECLARE item_category_name VARCHAR(255);
+
+    SELECT i.market_price_max, i.market_price_min, i.item_category, ic.item_category_name
+    INTO item_market_price_max, item_market_price_min, item_category, item_category_name
+    FROM items i
+    JOIN item_categories ic ON i.item_category = ic.item_category_id
+    WHERE i.item_id = NEW.item_id;
+
+    IF NOT EXISTS (SELECT * FROM pawnbroker_specialization WHERE pawnbroker_id = NEW.pawnbroker_id AND specialization = item_category) THEN
+        SET @errorMessage = CONCAT('Pawnbroker with id \'', NEW.pawnbroker_id, '\' is not specialized in the item category - ', item_category_name);
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = @errorMessage;
+    ELSEIF (NEW.pawn_amount > item_market_price_max OR NEW.pawn_amount < item_market_price_min) THEN
+        SET @errorMessage = CONCAT('Invalid pawn amount value, should be between ', item_market_price_min, ' and ', item_market_price_max);
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = @errorMessage;
+    END IF;
+END $$
+
+DELIMITER ;
---
+++ src/main/resources/db/migration/V4__repayments_after_insert.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+
+CREATE TRIGGER repayments_after_insert
+    AFTER INSERT ON repayments
+    FOR EACH ROW
+BEGIN
+    UPDATE pawn_transactions
+    SET transaction_status = 'Repaid'
+    WHERE transaction_id = NEW.transaction_id AND transaction_status = 'Active';
+END $$
+
+DELIMITER ;
